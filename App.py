b3b6b73c53bc747c082789238df6b6decfb3d576
Felipe Rocha <rochadsf@gmail.com>
2020-07-30 23:24:56 -0300

    Finalização Menu Moeda e Valor Entrada

--- App.py
+++ App.py
@@ -1,45 +1,35 @@
+valorEntradaMoeda = 0.00
+
 def menuMoeda():
-    escolhaMenu = input('Selecione uma moeda digitando seu número:' 
+    global escolha_menu
+    escolha_menu = int(input('Selecione uma moeda digitando seu número:' 
                 '\n1 - Real'
-                '\n2-  Dólar'
-                '\n3-  Euro\n')
+                '\n2 -  Dólar'
+                '\n3 -  Euro\n'))
 
 menuMoeda()
 
-print(1 , escolhaMenu)
-
-valorEntradaMoeda = 0.00
-
-def valorInicial(escolhaMenu):
-    while escolhaMenu == '1' or '2' or '3':
-        if escolhaMenu == '1':
+def valorInicial():
+    while escolha_menu > 3 or escolha_menu < 1:
+        print('Moeda inválida!')
+        menuMoeda()
+    else:
+        if escolha_menu == 1:
             valorEntrada = float(input('Informe o valor inicial: R$ '))
             valorEntradaMoeda = round(valorEntrada, 2)
             print(valorEntradaMoeda)
-            print(type(valorEntradaMoeda))
 
-        if escolhaMenu == '2':
+        if escolha_menu == 2:
             valorEntrada = float(input('Informe o valor inicial: $ '))
             valorEntradaMoeda = round(valorEntrada, 2)
             print(valorEntradaMoeda)
-            print(type(valorEntradaMoeda))
 
-        if escolhaMenu == '3':
+        if escolha_menu == 3:
             valorEntrada = float(input('Informe o valor inicial: € '))
             valorEntradaMoeda = round(valorEntrada, 2)
             print(valorEntradaMoeda)
-            print(type(valorEntradaMoeda))
-    else:
-        print('Moeda inválida')
-        menuMoeda()
 
 valorInicial()
-
-print(type(valorEntradaMoeda))
-
-soma = 20 + valorEntradaMoeda
-
-print(soma)
 
 
 
